b223d96e32843767ecd2cc4ff35cf58eabcd06a9
yoon-0012 <dbzho8383@kpu.ac.kr>
2022-07-09 19:50:49 +0900

    방학

---
+++ 0709/static_cast03.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
+class Base
+{
+public:
+	virtual void dummy()
+	{
+		std::cout << "Base dummy() È£Ãâ.\n";
+	}
+};
+
+class Derived :public Base
+{
+public:
+	void SetNumber(int i)
+	{
+		number = i;
+	}
+	int number;
+};
+
+int main()
+{
+	Derived d;
+	Base& br = d;
+
+	Derived& another_d = static_cast<Derived&>(br);
+	printf("d = %p, br = %p, another_d = %p\n", &d, &br, &another_d);
+
+	Derived a[10];
+	Base* dp = static_cast<Base*>(a);
+	printf("a = %p, dp = %p\n", a, dp);
+}
